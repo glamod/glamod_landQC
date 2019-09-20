7054c74ec8f90cb27fa1b68900d0e66c35746e5b
Robert Dunn <robert.dunn@metoffice.gov.uk>
2019-09-20 11:55:51 +0100

    Added command line switch to be able to select individual tests during validation and development.

--- intra_checks.py
+++ intra_checks.py
@@ -3,7 +3,7 @@ Intra station checks (within station record)
 
 intra_checks.py invoked by typing::
 
-  python intra_checks.py --restart_id --end_id [--full] [--plots] [--diagnostics]
+  python intra_checks.py --restart_id --end_id [--full] [--plots] [--diagnostics] [--test]
 
 Input arguments:
 
@@ -16,6 +16,9 @@ Input arguments:
 --plots             [False] Create plots (maybe interactive)
 
 --diagnostics       [False] Verbose output
+
+--test              ["all"] select a single test to run [climatological/distribution/diurnal
+                     frequent/humidity/odd_cluster/pressure/spike/streaks/timestamp/variance/winds/world_records]
 '''
 #************************************************************************
 import os
@@ -37,7 +40,7 @@ CONF_LOC = setup.SUBDAILY_CONFIG_DIR
 
 
 #************************************************************************
-def run_checks(restart_id="", end_id="", diagnostics=False, plots=False, full=False):
+def run_checks(restart_id="", end_id="", diagnostics=False, plots=False, full=False, test="all"):
     """
     Main script.  Reads in station data, populates internal objects and passes to the tests.
 
@@ -46,6 +49,8 @@ def run_checks(restart_id="", end_id="", diagnostics=False, plots=False, full=Fa
     :param bool diagnostics: print extra material to screen
     :param bool plots: create plots from each test
     :param bool full: run full reprocessing rather than using stored values.
+    :param str test: specify a single test to run (useful for diagnostics) [climatological/distribution/diurnal
+                     frequent/humidity/odd_cluster/pressure/spike/streaks/timestamp/variance/winds/world_records]
     """
 
     obs_var_list = setup.obs_var_list
@@ -128,50 +133,63 @@ def run_checks(restart_id="", end_id="", diagnostics=False, plots=False, full=Fa
         Precipitation logical checks - precip not in C3S 311a @Aug 2019
         """
         #*************************
-        print("O", dt.datetime.now()-startT)
-        # TODO - use suite config file to store all settings for tests
-        qc_tests.odd_cluster.occ(station, ["temperature", "dew_point_temperature", "station_level_pressure", "sea_level_pressure", "wind_speed"], config_file, full=full, plots=plots, diagnostics=diagnostics)
+        if test in ["all", "odd_cluster"]:
+            print("O", dt.datetime.now()-startT)
+            # TODO - use suite config file to store all settings for tests
+            qc_tests.odd_cluster.occ(station, ["temperature", "dew_point_temperature", "station_level_pressure", "sea_level_pressure", "wind_speed"], config_file, full=full, plots=plots, diagnostics=diagnostics)
 
-        print("F", dt.datetime.now()-startT)
-        qc_tests.frequent.fvc(station, ["temperature", "dew_point_temperature", "station_level_pressure", "sea_level_pressure"], config_file, full=full, plots=plots, diagnostics=diagnostics)
+        if test in ["all", "frequent"]:
+            print("F", dt.datetime.now()-startT)
+            qc_tests.frequent.fvc(station, ["temperature", "dew_point_temperature", "station_level_pressure", "sea_level_pressure"], config_file, full=full, plots=plots, diagnostics=diagnostics)
 
         # HadISD only runs on stations where latitude higher than 60(N/S)
         # Takes a long time, this one
-#        print("U", dt.datetime.now()-startT)
-#        if np.abs(station.latitude < 60):
-#            qc_tests.diurnal.dcc(station, config_file, full=full, plots=plots, diagnostics=diagnostics)
+#        if test in ["all", "diurnal"]:
+#            print("U", dt.datetime.now()-startT)
+#            if np.abs(station.latitude < 60):
+#                qc_tests.diurnal.dcc(station, config_file, full=full, plots=plots, diagnostics=diagnostics)
 
-        print("D", dt.datetime.now()-startT)
-        qc_tests.distribution.dgc(station, ["temperature", "dew_point_temperature", "station_level_pressure", "sea_level_pressure"], config_file, full=full, plots=plots, diagnostics=diagnostics)
+        if test in ["all", "distribution"]:
+            print("D", dt.datetime.now()-startT)
+            qc_tests.distribution.dgc(station, ["temperature", "dew_point_temperature", "station_level_pressure", "sea_level_pressure"], config_file, full=full, plots=plots, diagnostics=diagnostics)
 
-        print("W", dt.datetime.now()-startT)
-        qc_tests.world_records.wrc(station, ["temperature", "dew_point_temperature", "sea_level_pressure", "wind_speed"], full=full, plots=plots, diagnostics=diagnostics)
+        if test in ["all", "world_records"]:
+            print("W", dt.datetime.now()-startT)
+            qc_tests.world_records.wrc(station, ["temperature", "dew_point_temperature", "sea_level_pressure", "wind_speed"], full=full, plots=plots, diagnostics=diagnostics)
 
         # could run on wind direction?
-        print("K", dt.datetime.now()-startT)
-        qc_tests.streaks.rsc(station, ["temperature", "dew_point_temperature", "station_level_pressure", "sea_level_pressure", "wind_speed"], config_file, full=full, plots=plots, diagnostics=diagnostics)
+        if test in ["all", "streaks"]:
+            print("K", dt.datetime.now()-startT)
+            qc_tests.streaks.rsc(station, ["temperature", "dew_point_temperature", "station_level_pressure", "sea_level_pressure", "wind_speed"], config_file, full=full, plots=plots, diagnostics=diagnostics)
 
         # not run on pressure data in HadISD.
-        print("C", dt.datetime.now()-startT)
-        qc_tests.climatological.coc(station, ["temperature", "dew_point_temperature"], config_file, full=full, plots=plots, diagnostics=diagnostics)
+        if test in ["all", "climatological"]:
+            print("C", dt.datetime.now()-startT)
+            qc_tests.climatological.coc(station, ["temperature", "dew_point_temperature"], config_file, full=full, plots=plots, diagnostics=diagnostics)
 
-        print("T", dt.datetime.now()-startT)
-        qc_tests.timestamp.tsc(station, ["temperature", "dew_point_temperature", "station_level_pressure", "sea_level_pressure", "wind_speed"], config_file, full=full, plots=plots, diagnostics=diagnostics)
+        if test in ["all", "timestamp"]:
+            print("T", dt.datetime.now()-startT)
+            qc_tests.timestamp.tsc(station, ["temperature", "dew_point_temperature", "station_level_pressure", "sea_level_pressure", "wind_speed"], config_file, full=full, plots=plots, diagnostics=diagnostics)
 
-        print("S", dt.datetime.now()-startT)
-        qc_tests.spike.sc(station, ["temperature", "dew_point_temperature", "station_level_pressure", "sea_level_pressure", "wind_speed"], config_file, full=full, plots=plots, diagnostics=diagnostics)
+        if test in ["all", "spike"]:
+            print("S", dt.datetime.now()-startT)
+            qc_tests.spike.sc(station, ["temperature", "dew_point_temperature", "station_level_pressure", "sea_level_pressure", "wind_speed"], config_file, full=full, plots=plots, diagnostics=diagnostics)
 
-        print("H", dt.datetime.now()-startT)
-        qc_tests.humidity.hcc(station, config_file, full=full, plots=plots, diagnostics=diagnostics)
+        if test in ["all", "humidity"]:
+            print("H", dt.datetime.now()-startT)
+            qc_tests.humidity.hcc(station, config_file, full=full, plots=plots, diagnostics=diagnostics)
 
-        print("V", dt.datetime.now()-startT)
-        qc_tests.variance.evc(station, ["temperature", "dew_point_temperature", "station_level_pressure", "sea_level_pressure", "wind_speed"], config_file, full=full, plots=plots, diagnostics=diagnostics)
+        if test in ["all", "variance"]:
+            print("V", dt.datetime.now()-startT)
+            qc_tests.variance.evc(station, ["temperature", "dew_point_temperature", "station_level_pressure", "sea_level_pressure", "wind_speed"], config_file, full=full, plots=plots, diagnostics=diagnostics)
 
-        print("P", dt.datetime.now()-startT)
-        qc_tests.pressure.pcc(station, config_file, full=full, plots=plots, diagnostics=diagnostics)
+        if test in ["all", "pressure"]:
+            print("P", dt.datetime.now()-startT)
+            qc_tests.pressure.pcc(station, config_file, full=full, plots=plots, diagnostics=diagnostics)
 
-        print("w", dt.datetime.now()-startT)
-        qc_tests.winds.wcc(station, config_file, fix=True, full=full, plots=plots, diagnostics=diagnostics)
+        if test in ["all", "winds"]:
+            print("w", dt.datetime.now()-startT)
+            qc_tests.winds.wcc(station, config_file, fix=True, full=full, plots=plots, diagnostics=diagnostics)
 
         print(dt.datetime.now()-startT)
 
@@ -227,12 +245,15 @@ if __name__ == "__main__":
                         help='Run diagnostics (will not write out file)')
     parser.add_argument('--plots', dest='plots', action='store_true', default=False,
                         help='Run plots (will not write out file)')
+    parser.add_argument('--test', dest='test', action='store', default="all",
+                        help='Select single test [climatological/distribution/diurnal/frequent/humidity/odd_cluster/pressure/spike/streaks/timestamp/variance/winds/world_records]')
     args = parser.parse_args()
 
     run_checks(restart_id=args.restart_id,
                end_id=args.end_id,
                diagnostics=args.diagnostics,
                plots=args.plots,
-               full=args.full)
+               full=args.full,
+               test=args.test)
 
 #************************************************************************
