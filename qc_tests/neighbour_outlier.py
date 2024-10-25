99be3471ae1a166e3173f1339ed0ddd609b06b1e
Robert Dunn <robert.dunn@metoffice.gov.uk>
2024-10-25 15:08:20 +0100

    #155: tests on reading in buddies

--- qc_tests/neighbour_outlier.py
+++ qc_tests/neighbour_outlier.py
@@ -20,13 +20,13 @@ import setup
 
 MIN_SPREAD = 2 # TODO should this be different for each variable?
 SPREAD_LIMIT = 5 # matches HadISD which used 5*IQR
-DUBIOUS_FRACTION = 0.66 # more than 2/3 of neighbours suggest differences dubious
+DUBIOUS_FRACTION = 0.667 # more than 2/3 of neighbours suggest differences dubious
 STORM_FRACTION = 0.667 # fraction of negative differences to target (likely at centre of low pressure system)
 
 
 #************************************************************************
-def plot_neighbour_flags(times: np.array, flagged_time: int,
-                         target: utils.Meteorological_Variable, buddies: np.array) -> None:
+def plot_neighbour_flags(times: np.ndarray, flagged_time: int,
+                         target: utils.Meteorological_Variable, buddies: np.ndarray) -> None:
     '''
     Plot each spike against surrounding data
 
@@ -60,8 +60,8 @@ def plot_neighbour_flags(times: np.array, flagged_time: int,
 
 #************************************************************************
 def read_in_buddies(target_station: utils.Station, station_list: pd.DataFrame, 
-                    initial_neighbours: np.array, variable: utils.Meteorological_Variable,
-                    diagnostics: bool = False, plots: bool = False) -> tuple[int, np.array]:
+                    initial_neighbours: np.ndarray, variable: utils.Meteorological_Variable,
+                    diagnostics: bool = False, plots: bool = False) -> np.ndarray:
     """
     Read in the buddy data for the neighbours
 
@@ -72,7 +72,7 @@ def read_in_buddies(target_station: utils.Station, station_list: pd.DataFrame,
     :param bool diagnostics: print extra material to screen
     :param bool plots: create plots from each test
 
-    :returns: (int, array) number of buddies, and the buddy data
+    :returns: array of the buddy data
     
     """
     #*************************
@@ -90,7 +90,7 @@ def read_in_buddies(target_station: utils.Station, station_list: pd.DataFrame,
             break
 
         if diagnostics:
-            print(f"Buddy number {bid}/{len(initial_neighbours[:, 0])} {buddy_id}")
+            print(f"Buddy number {bid+1}/{len(initial_neighbours[:, 0])} {buddy_id}")
 
         # set up station object to hold information
         buddy_idx, = np.where(station_list.id == buddy_id)
@@ -99,14 +99,14 @@ def read_in_buddies(target_station: utils.Station, station_list: pd.DataFrame,
                                 station_list.iloc[buddy_idx].elevation.values[0])
 
         try:
-            buddy, buddy_df = io.read_station(os.path.join(setup.SUBDAILY_PROC_DIR,
-                                                            f"{buddy_id:11s}.qff{setup.OUT_COMPRESSION}"),
-                                                buddy, read_flags=True) 
+            buddy, _ = io.read_station(os.path.join(setup.SUBDAILY_PROC_DIR,
+                                              f"{buddy_id:11s}.qff{setup.OUT_COMPRESSION}"),
+                                              buddy, read_flags=True) 
 
             buddy_var = getattr(buddy, variable)
 
             # apply flags
-            flag_locs, = np.where(buddy_var.flags != "") 
+            flag_locs, = np.nonzero(buddy_var.flags != "") 
             buddy_var.data.mask[flag_locs] = True
 
         except OSError: # as e:
@@ -128,27 +128,31 @@ def read_in_buddies(target_station: utils.Station, station_list: pd.DataFrame,
             buddy_count += 1
 
     logger.info("All buddies read in")
+    if buddy_count < utils.MIN_NEIGHBOURS:
+        logger.warning(f"Insufficient number of neighbours with data ({buddy_count} < {utils.MIN_NEIGHBOURS})")
+    else:
+        logger.info(f"Number of neighbours with data: {buddy_count}")
 
-    return buddy_count, all_buddy_data
+    return all_buddy_data
 
 
 #************************************************************************
 def calculate_data_spread(target_station: utils.Station,
-                          differences: np.array) -> np.array:
+                          differences: np.ndarray) -> np.ndarray:
     """
     Find spread of differences on monthly basis (with minimum value)  
 
     :param Station target_station: station to run on    
-    :param np.array differences: array of differences of buddy data to target data
+    :param np.ndarray differences: array of differences of buddy data to target data
     
-    :returns: np.array of the spread of the differences
+    :returns: np.ndarray of the spread of the differences
     """
 
     spreads = np.ma.zeros(differences.shape)
 
     for month in range(1, 13):
 
-        month_locs = np.where(target_station.months == month)
+        month_locs = np.nonzero(target_station.months == month)
 
         for bid, buddy in enumerate(differences):
 
@@ -158,7 +162,7 @@ def calculate_data_spread(target_station: utils.Station,
                 if this_spread < MIN_SPREAD:
                     spreads[bid, month_locs] = MIN_SPREAD
                 else:
-                    spreads[bid, month_locs] = utils.spread(differences[bid, month_locs])
+                    spreads[bid, month_locs] = this_spread
 
             else:
                 spreads[bid, month_locs] = MIN_SPREAD
@@ -168,18 +172,18 @@ def calculate_data_spread(target_station: utils.Station,
 
 
 #************************************************************************
-def adjust_pressure_for_tropical_storms(dubious: np.ma.array, initial_neighbours: np.array,
-                                        differences: np.array, spreads: np.array) -> np.array:
+def adjust_pressure_for_tropical_storms(dubious: np.ma.MaskedArray, initial_neighbours: np.ndarray,
+                                        differences: np.ndarray, spreads: np.ndarray) -> np.ndarray:
     """
     For pressure variables look for tropical storm passages, where large differences
     of station under the "eye" to neighbours are real and expected.
     
-    :param np.ma.array dubious: locations where differences are greater than expected
+    :param np.ma.MaskedArray dubious: locations where differences are greater than expected
     :param array initial_neighbours: input neighbours (ID, distance) pairs
     :param array differences: array of differences between target and neighbours
     :param array spreads: array of the spread of the differences
 
-    :returns: np.array of locations where target values are dubious given the neighbours  
+    :returns: np.ndarray of locations where target values are dubious given the neighbours  
     """
 
     distant, = np.where(initial_neighbours[:, 1].astype(int) > 100)
@@ -210,7 +214,7 @@ def adjust_pressure_for_tropical_storms(dubious: np.ma.array, initial_neighbours
 
 
 #************************************************************************
-def neighbour_outlier(target_station: utils.Station, initial_neighbours: np.array,
+def neighbour_outlier(target_station: utils.Station, initial_neighbours: np.ndarray,
                       variable: utils.Meteorological_Variable, diagnostics: bool = False,
                       plots: bool = False, full: bool = False) -> None:
     """
@@ -226,15 +230,6 @@ def neighbour_outlier(target_station: utils.Station, initial_neighbours: np.arra
     """
     station_list = utils.get_station_list()
 
-    # if sufficient
-    n_neighbours = len(np.where(initial_neighbours[:, 0] != "-")[0])-1
-    if n_neighbours < utils.MIN_NEIGHBOURS:
-        logger.warning(f"{target_station.id} has insufficient neighbours ({n_neighbours}<{utils.MIN_NEIGHBOURS})")
-        print(f"{target_station.id} has insufficient neighbours ({n_neighbours}<{utils.MIN_NEIGHBOURS})")
-        return
-
-    logger.info(f"{target_station.id} has {n_neighbours} neighbours")
-
     #*************************
     # extract target observations
     obs_var = getattr(target_station, variable)
@@ -242,13 +237,8 @@ def neighbour_outlier(target_station: utils.Station, initial_neighbours: np.arra
 
     #*************************
     # Read in the buddy data
-    buddy_count, all_buddy_data = read_in_buddies(target_station, station_list, initial_neighbours,
-                                                  variable, diagnostics=diagnostics, plots=plots)
-
-    if buddy_count < utils.MIN_NEIGHBOURS:
-        logger.warning(f"Insufficient number of neighbours with data ({buddy_count} < {utils.MIN_NEIGHBOURS})")
-    else:
-        logger.info(f"Number of neighbours with data: {buddy_count}")
+    all_buddy_data = read_in_buddies(target_station, station_list, initial_neighbours,
+                                     variable, diagnostics=diagnostics, plots=plots)
 
     #*************************
     # find differences
@@ -267,7 +257,6 @@ def neighbour_outlier(target_station: utils.Station, initial_neighbours: np.arra
     if variable in ["sea_level_pressure", "station_level_pressure"]:
         dubious = adjust_pressure_for_tropical_storms(dubious, initial_neighbours,
                                                       differences, spreads)
-
     else:
         #*************************
         # note where differences exceed the spread [all non pressure variables]
@@ -298,7 +287,7 @@ def neighbour_outlier(target_station: utils.Station, initial_neighbours: np.arra
     return # neighbour_outlier
 
 #************************************************************************
-def noc(target_station: utils.Station, initial_neighbours: np.array, var_list: list,
+def noc(target_station: utils.Station, initial_neighbours: np.ndarray, var_list: list,
         full: bool = False, plots: bool = False, diagnostics: bool = False) -> None:
     """
     Run through the variables and pass to the Neighbour Outlier Check
@@ -311,12 +300,22 @@ def noc(target_station: utils.Station, initial_neighbours: np.array, var_list: l
     :param bool diagnostics: turn on diagnostic output
     """
 
-    for var in var_list:
-        # TODO: remove duplicated read step (reading neighbours for each variable!)
-        neighbour_outlier(target_station, initial_neighbours, var,
-                          diagnostics=diagnostics, plots=plots, full=full)
+    # Check if have sufficient neighbours, as if too few, might as well exit here.
+    #    First entry is self (target station), hence the "-1" at the end.
+    n_neighbours = len(np.where(initial_neighbours[:, 0] != "-")[0]) - 1
+    if n_neighbours < utils.MIN_NEIGHBOURS:
+        logger.warning(f"{target_station.id} has insufficient neighbours ({n_neighbours}<{utils.MIN_NEIGHBOURS})")
+        # print(f"{target_station.id} has insufficient neighbours ({n_neighbours}<{utils.MIN_NEIGHBOURS})")
 
-    return # noc
+    else:
+        logger.info(f"{target_station.id} has {n_neighbours} neighbours")
+
+        for var in var_list:
+            # TODO: remove duplicated read step (reading neighbours for each variable!)
+            neighbour_outlier(target_station, initial_neighbours, var,
+                            diagnostics=diagnostics, plots=plots, full=full)
+
+    # noc
 
 #************************************************************************
 if __name__ == "__main__":
--- tests/test_qc_tests/test_neighbour_outlier.py
+++ tests/test_qc_tests/test_neighbour_outlier.py
@@ -2,7 +2,7 @@
 Contains tests for neighour_outlier.py
 """
 import numpy as np
-import datetime as dt
+import pandas as pd
 import pytest
 from unittest.mock import patch, Mock
 
@@ -13,7 +13,116 @@ import common
 
 # not testing plotting
 
-#def test_read_in_buddies() -> None:
+@patch("neighbour_outlier.io.read_station")
+def test_read_in_buddies_short(read_station_mock: Mock) -> None:
+
+    # set up variables and stations
+    temperatures = common.example_test_variable("temperature", np.arange(10))
+    target_station = common.example_test_station(temperatures)
+    target_station.id = "Target"
+    buddy_station = common.example_test_station(temperatures)
+    buddy_station.id = "Buddy"
+
+    read_station_mock.return_value = (buddy_station , None)
+
+    instring = [["Target", 45, 100, 10],
+                ["Buddy", 45, 100, 10],
+                ]
+    station_list = pd.DataFrame(instring, columns=["id", "latitude", "longitude", "elevation"])
+
+    buddy_list = np.array([["Target", 0], ["Buddy", 20]])
+
+    buddy_data = neighbour_outlier.read_in_buddies(target_station, station_list,
+                                                   buddy_list, "temperature", diagnostics=True)
+
+    # first entry is blank (for self)
+    assert len(buddy_data[0].compressed()) == 0
+
+    # second should contain buddy data
+    np.testing.assert_array_equal(buddy_data[1], np.ma.arange(10))
+
+
+@patch("neighbour_outlier.io.read_station")
+def test_read_in_buddies(read_station_mock: Mock) -> None:
+
+    # set up variables and stations
+    temperatures = common.example_test_variable("temperature", np.arange(10))
+    target_station = common.example_test_station(temperatures)
+    target_station.id = "Target"
+    buddy_station = common.example_test_station(temperatures)
+    buddy_station.id = "Buddy"
+
+    read_station_mock.return_value = (buddy_station , None)
+
+    instring = [["Target", 45, 100, 10],
+                ["Buddy", 45, 100, 10],
+                ]
+    station_list = pd.DataFrame(instring, columns=["id", "latitude", "longitude", "elevation"])
+
+    buddy_list = np.array([["Target", 0], ["Buddy", 20]])
+
+    buddy_data = neighbour_outlier.read_in_buddies(target_station, station_list,
+                                                   buddy_list, "temperature", diagnostics=True)
+
+    # first entry is blank (for self)
+    assert len(buddy_data[0].compressed()) == 0
+
+    # second should contain buddy data
+    np.testing.assert_array_equal(buddy_data[1], np.ma.arange(10))
+
+@patch("neighbour_outlier.io")
+def test_read_in_buddies_oserror(io_mock: Mock) -> None:
+
+    # set up variables and stations
+    temperatures = common.example_test_variable("temperature", np.arange(10))
+    target_station = common.example_test_station(temperatures)
+    target_station.id = "Target"
+    buddy_station = common.example_test_station(temperatures)
+    buddy_station.id = "Buddy"
+
+    io_mock.read_station.side_effect = OSError
+
+    instring = [["Target", 45, 100, 10],
+                ["Buddy", 45, 100, 10],
+                ]
+    station_list = pd.DataFrame(instring, columns=["id", "latitude", "longitude", "elevation"])
+
+    buddy_list = np.array([["Target", 0], ["Buddy", 20]])
+
+    buddy_data = neighbour_outlier.read_in_buddies(target_station, station_list,
+                                                   buddy_list, "temperature", diagnostics=True)
+
+    # Check error handling
+    io_mock.write_error.assert_called_once_with(target_station,
+                                                "File Missing (Buddy, temperature) - Buddy")
+
+
+@patch("neighbour_outlier.io")
+def test_read_in_buddies_valueerror(io_mock: Mock) -> None:
+
+    # set up variables and stations
+    temperatures = common.example_test_variable("temperature", np.arange(10))
+    target_station = common.example_test_station(temperatures)
+    target_station.id = "Target"
+    buddy_station = common.example_test_station(temperatures)
+    buddy_station.id = "Buddy"
+
+    io_mock.read_station.side_effect = ValueError("error text")
+
+    instring = [["Target", 45, 100, 10],
+                ["Buddy", 45, 100, 10],
+                ]
+    station_list = pd.DataFrame(instring, columns=["id", "latitude", "longitude", "elevation"])
+
+    buddy_list = np.array([["Target", 0], ["Buddy", 20]])
+
+    buddy_data = neighbour_outlier.read_in_buddies(target_station, station_list,
+                                                   buddy_list, "temperature", diagnostics=True)
+
+    # Check error handling
+    io_mock.write_error.assert_called_once_with(target_station,
+                                                "Error in input file (Buddy, temperature) - Buddy",
+                                                error="error text")
 
 
 #def test_calculate_data_spread() -> None:
@@ -33,8 +142,25 @@ def test_noc(neighbour_outlier_mock: Mock) -> None:
     temperatures = common.example_test_variable("temperature", np.arange(10))
     station = common.example_test_station(temperatures)
 
-    # Do the call
-    neighbour_outlier.noc(station, np.array(["ID", 20]), ["temperature"])
+    # Do the call, with 4 neighbours
+    neighbour_outlier.noc(station, np.array([["ID1", 20],
+                                             ["ID2", 30],
+                                             ["ID3", 40],
+                                             ["ID4", 50],]), ["temperature"])
 
     # Mock to check call occurs as expected with right return
     neighbour_outlier_mock.assert_called_once()
+
+
+@patch("neighbour_outlier.neighbour_outlier")
+def test_noc_few_buddies(neighbour_outlier_mock: Mock) -> None:
+
+    # Set up data, variable & station
+    temperatures = common.example_test_variable("temperature", np.arange(10))
+    station = common.example_test_station(temperatures)
+
+    # Do the call with one neighbour
+    neighbour_outlier.noc(station, np.array([["ID", 20]]), ["temperature"])
+
+    # Mock to check call wasn't made
+    neighbour_outlier_mock.assert_not_called()
