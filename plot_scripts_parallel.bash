e8fd4ecee1cd5f98c6afa98497ea923403ecf0e6
Robert Dunn <robert.dunn@metoffice.gov.uk>
2024-11-25 10:38:55 +0000

    #183: typos in scripts fixed

--- plot_scripts_parallel.bash
+++ plot_scripts_parallel.bash
@@ -5,22 +5,27 @@ function write_and_submit_bastion_script {
     parallel_script=${1}
 
     # generate a "screen" instance in detached mode
-    screen -S "metadata" -d -m
+    screen -S "plots" -d -m
 
     # run the parallel script in this detached screen
-    screen -r "metadata" -X stuff $'conda activate glamod_QC \n'
+    screen -r "plots" -X stuff $'conda activate glamod_QC \n'
 
     # run the parallel script in this detached screen
     #  restrict to 1 job so that these are done in sequence
-    screen -r "metadata" -X stuff $"parallel --jobs 1 < ${parallel_script} "
+    screen -r "plots" -X stuff $"parallel --jobs 1 < ${parallel_script} "
 
 
 } # write_and_submit_bastion_script
 
-parallel_script="${SCRIPT_DIR}/parallel_metadata.bash"
+cwd=$(pwd)
+SCRIPT_DIR=${cwd}/parallel_scripts/
 
-echo "conda activate glamod_QC" > ${parallel_script}
-echo "" > ${parallel_script}
-echo "python plot_map_of_flagging_rates.py" > ${parallel_script}
+parallel_script="${SCRIPT_DIR}/parallel_plots.bash"
+
+if [ -e "${parallel_script}" ]; then
+    rm "${parallel_script}"
+fi
+
+echo "python plot_map_of_flagging_rates.py" >> ${parallel_script}
 
 write_and_submit_bastion_script "${parallel_script}"
