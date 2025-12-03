1f4943c6e600d3003276191d45933053a63e9650
Robert Dunn <robert.dunn@metoffice.gov.uk>
2025-12-03 14:46:52 +0000

    #253: removing duplication and tidying up

--- tests/test_qc_tests/test_frequent.py
+++ tests/test_qc_tests/test_frequent.py
@@ -5,11 +5,10 @@ import numpy as np
 import datetime as dt
 import pandas as pd
 import pytest
-from unittest.mock import (call, patch, mock_open, Mock)
+from unittest.mock import (patch, Mock)
 
 import frequent
 import utils
-import setup
 
 import common
 
@@ -35,6 +34,46 @@ def _setup_station(indata: np.ma.array) -> utils.Station:
 
     # make Station
     station = common.example_test_station(temperature)
+
+    return station
+
+
+def _setup_gaussian_station() -> utils.Station:
+    """Create a station with random gaussianly distributed
+    temperatures for 10 Januaries
+
+    Returns
+    -------
+    utils.Station
+        Station object with data in temperature field
+    """
+
+    nyears = 10
+    month_hours = 24*31
+    # 10 years of Januaries at 0.1C resolution
+    indata = np.ma.array(np.round(np.random.normal(15, 10,
+                                                   month_hours * nyears),
+                                  decimals=1))
+    indata.mask = np.zeros(len(indata))
+    # every 5th entry set to 0 (mean is 15)
+    indata[::5] = 0
+
+    # make MetVars
+    temperature = common.example_test_variable("temperature", indata)
+
+    for y in range(nyears):
+        start_dt = dt.datetime(2000+y, 1, 1, 0, 0)
+        month_times = pd.to_datetime(pd.DataFrame([start_dt + dt.timedelta(hours=i)\
+                                for i in range(month_hours)])[0])
+        if y == 0:
+            times = month_times.copy()
+        else:
+            times = pd.concat([times, month_times])
+    # check at this generation stage that have all Januaries
+    assert np.unique(times.dt.month) == 1
+
+    # make Station, by hand so can set times
+    station = common.example_test_station(temperature, times)
 
     return station
 
@@ -79,8 +118,8 @@ def test_scan_histogram(bad_value: int,
 @pytest.mark.parametrize("bad_value, expected", ([400, []],
                                                  [1200, []],
                                                  [1201, [3]]))
-def test_scan_histogram(bad_value: int,
-                        expected: list) -> None:
+def test_scan_histogram_slope(bad_value: int,
+                              expected: list) -> None:
     """Test that peaks are pulled out as expected"""
 
     bins = np.arange(8)  # test just central vs the rest
@@ -126,84 +165,38 @@ def test_identify_values() -> None:
 
     frequent.identify_values(obs_var, station, config_dict)
 
-    assert config_dict["FREQUENT-temperature"][f"1-width"] == "1.0"
-    assert config_dict["FREQUENT-temperature"][f"1"] == [10]
+    assert config_dict["FREQUENT-temperature"]["1-width"] == "1.0"
+    assert config_dict["FREQUENT-temperature"]["1"] == [10]
 
 
 def test_identify_values_gaussian() -> None:
     """Check identification works using gaussian data"""
-    nyears = 10
-    month_hours = 24*31
-    # 10 years of Januaries at 0.1C resolution
-    indata = np.ma.array(np.round(np.random.normal(15, 10,
-                                                   month_hours * nyears),
-                                  decimals=1))
-    indata.mask = np.zeros(len(indata))
-    # every 5th entry set to 0 (mean is 15)
-    indata[::5] = 0
 
-    # make MetVars
-    temperature = common.example_test_variable("temperature", indata)
-
-    for y in range(nyears):
-        start_dt = dt.datetime(2000+y, 1, 1, 0, 0)
-        month_times = pd.to_datetime(pd.DataFrame([start_dt + dt.timedelta(hours=i)\
-                                for i in range(month_hours)])[0])
-        if y == 0:
-            times = month_times.copy()
-        else:
-            times = pd.concat([times, month_times])
-    # check at this generation stage that have all Januaries
-    assert np.unique(times.dt.month) == 1
-
-    # make Station, by hand so can set times
-    station = common.example_test_station(temperature, times)
+    station = _setup_gaussian_station()
+    temperature = station.temperature
 
     config_dict = {}
 
     frequent.identify_values(temperature, station, config_dict)
 
     # data resolution at 0.1 so width == 0.5
-    assert config_dict["FREQUENT-temperature"][f"1-width"] == "0.5"
-    assert config_dict["FREQUENT-temperature"][f"1"] == [0]
+    assert config_dict["FREQUENT-temperature"]["1-width"] == "0.5"
+    assert config_dict["FREQUENT-temperature"]["1"] == [0]
 
 
 def test_frequent_values() -> None:
+    """Test overall script with example data"""
 
-    nyears = 10
-    month_hours = 24*31
-    # 10 years of Januaries at 0.1C resolution
-    indata = np.ma.array(np.round(np.random.normal(15, 10,
-                                                   month_hours * nyears),
-                                  decimals=1))
-    indata.mask = np.zeros(len(indata))
-    # every 5th entry set to 0 (mean is 15)
-    indata[::5] = 0
-
-    # make MetVars
-    temperature = common.example_test_variable("temperature", indata)
-
-    for y in range(nyears):
-        start_dt = dt.datetime(2000+y, 1, 1, 0, 0)
-        month_times = pd.to_datetime(pd.DataFrame([start_dt + dt.timedelta(hours=i)\
-                                for i in range(month_hours)])[0])
-        if y == 0:
-            times = month_times.copy()
-        else:
-            times = pd.concat([times, month_times])
-    # check at this generation stage that have all Januaries
-    assert np.unique(times.dt.month) == 1
-
-    # make Station, by hand so can set times
-    station = common.example_test_station(temperature, times)
+    station = _setup_gaussian_station()
+    temperature = station.temperature
 
     config_dict = {}
 
     frequent.frequent_values(temperature, station, config_dict)
 
     # data resolution at 0.1 so width == 0.5
-    assert config_dict["FREQUENT-temperature"][f"1-width"] == "0.5"
-    assert config_dict["FREQUENT-temperature"][f"1"] == [0]
+    assert config_dict["FREQUENT-temperature"]["1-width"] == "0.5"
+    assert config_dict["FREQUENT-temperature"]["1"] == [0]
 
     # assert flags set correctly
     flag_locs = np.nonzero(temperature.flags == "F")
@@ -217,6 +210,7 @@ def test_frequent_values() -> None:
 @patch("frequent.frequent_values")
 def test_fvc(identify_mock: Mock,
              values_mock: Mock) -> None:
+    """Test control script calls routines as expected"""
 
     station = _setup_station(np.ma.ones(24) * 10)
 
